4a5d4c2ccb2a962edf3a7876c7091c46adf755e8
Melanie <melanie-connolly@hotmail.com>
2020-03-07 11:22:28 +1000

    got second test passing woohoo

--- spec.js
+++ spec.js
@@ -1,23 +1,41 @@
 // spec.js
 url = "http://todomvc.com/examples/angularjs/#/"
 
-describe('To Do List App', function() {
-  it('should add item to todo list', function() {
-    //Open the designated url
+describe('To Do List App', function () {
+  //Open the designated url before each test 
+  beforeEach(function () {
     browser.get(url);
+  });
 
+  it('should add item to todo list', function () {
     //Add a new item to the list
     element(by.model('newTodo')).sendKeys('Buy Milk').sendKeys(protractor.Key.ENTER);
 
-    //Returns items in to do list
+    //Find item/s in to do list
     let items = element.all(by.className('todo-list')).all(by.className('ng-binding'));
+
+    //Confirm item is added
     expect(items.getText()).toEqual(['Buy Milk']);
 
   });
-});
 
-/*  it('should mark an item as complete', function () {
-    //Find an item and mark as complete 
-    element(by.cssContainingText('.ng-binding', 'Buy Milk')).click();
+  it('should mark an item as complete', function () {
+    //Add a new item to the list if required
+    element(by.model('newTodo')).sendKeys('Buy Chocolate').sendKeys(protractor.Key.ENTER);
+
+    //Find a specific item in to do list
+    let item = element.all(by.repeater('todo in todos')).filter(function (elem, index) {
+      return elem.getText().then(function (text) {
+        return text === 'Buy Chocolate';
+      });
+    }).first();
+
+    //Find and click checkbox next to item in to do list
+    item.element(by.model('todo.completed')).click(); 
+    browser.sleep(3000);
+
+    //Confirm item is marked as complete
+    expect(item.getAttribute('class')).toEqual('ng-scope completed');
+
   });
-}); */
+});
