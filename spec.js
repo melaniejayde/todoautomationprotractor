d255c294f1879a8de684b8c5ad646cc67a5e25f4
Melanie <melanie-connolly@hotmail.com>
2020-03-08 17:51:16 +1000

    Tidied the code

--- spec.js
+++ spec.js
@@ -1,22 +1,33 @@
 // spec.js
-url = "http://todomvc.com/examples/angularjs/#/"
-
 describe('To Do List App', function () {
+
+  //Defines URL to open angular web app
+  url = "http://todomvc.com/examples/angularjs/#/"
+
   //Open the designated url before each test 
   beforeEach(function () {
     browser.get(url);
   });
 
   //Reset browser between test cases
-  afterEach(function() {
+  afterEach(function () {
     browser.executeScript('window.sessionStorage.clear();');
     browser.executeScript('window.localStorage.clear();');
-});
+  });
 
   //Defines function to add a new item to the list
   function addItem(name) {
     element(by.model('newTodo')).sendKeys(name).sendKeys(protractor.Key.ENTER);
-     
+  };
+
+  //Defines function to retrieve all items in todolist
+  function getAllItems() {
+    return element.all(by.repeater('todo in todos'));
+  };
+
+  //Defines function to follow a link
+  function followLink(name) {
+    element(by.linkText(name)).click();
   };
 
   // Defines function to find item in list
@@ -32,7 +43,6 @@ describe('To Do List App', function () {
   function completeItem(name) {
     let item = findItem(name);
     item.element(by.model('todo.completed')).click();
-
   };
 
   //Defines function to delete item in list
@@ -44,21 +54,21 @@ describe('To Do List App', function () {
 
   it('should add item to todo list', function () {
     addItem('Buy Milk');
-
-    //Find item/s in to do list
-    let items = element.all(by.className('todo-list')).all(by.className('ng-binding'));
+    let item = findItem('Buy Milk')
 
     //Confirm item is added
-    expect(items.getText()).toEqual(['Buy Milk']);
-
+    expect(item.getText()).toEqual('Buy Milk');
   });
 
   it('should mark an item as complete', function () {
-    addItem('Buy Chocolate'); 
+    addItem('Buy Chocolate');
     completeItem('Buy Chocolate');
     let item = findItem('Buy Chocolate');
-    expect(item.getAttribute('class')).toEqual('ng-scope completed');
 
+    //Confirm item is in completed list
+    //If item is incomplete - class is "ng-scope"
+    //if item is complete - class is "ng-scope completed"
+    expect(item.getAttribute('class')).toEqual('ng-scope completed');
   });
 
   it('should see completed to do list', function () {
@@ -66,49 +76,42 @@ describe('To Do List App', function () {
     completeItem('Go to hairdressers');
 
     //Opens completed list 
-    let completed = element(by.linkText('Completed'))
-    completed.click();
+    followLink('Completed');
 
     //Verifies item is in completed to do list
-    let items = element.all(by.repeater('todo in todos'));
+    let items = getAllItems();
     expect(items.getText()).toEqual(['Go to hairdressers']);
-
   });
 
-  it('should remove item in to do list', function() {
+  it('should remove item in to do list', function () {
     addItem('Buy Coffee');
     deleteItem('Buy Coffee');
-    let items = element.all(by.repeater('todo in todos'));
+    let items = getAllItems();
 
     //Verify item was deleted 
     expect(items.getText()).not.toContain('Buy Coffee');
-
   });
 
-  it('should see count of remaining to do items', function() {
+  it('should see count of remaining to do items', function () {
     addItem('Mow Lawn');
     addItem('Do Groceries');
 
     //Opens link to active items in to do list
-    let active = element(by.linkText('Active'))
-    active.click();
+    followLink('Active');
 
     //Check the count shows x items
     let count = element.all(by.className('todo-count')).all(by.className('ng-binding'));
     expect(count.getText()).toEqual(['2']);
-  
   });
 
-  it('should see all to dos', function() {
+  it('should see all to dos', function () {
     addItem('Buy T-Shirt');
 
-    //Opens link to active items in to do list
-    let all = element(by.linkText('All'));
-    all.click();
+    //Opens link to all items in to do list
+    followLink('All');
+
+    //Verify item is in list of all items
     item = findItem('Buy T-Shirt');
     expect(item.getText()).toEqual('Buy T-Shirt');
-
   });
 });
-
-//protractor conf.js --grep='should see all to dos'
